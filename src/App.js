81fb6c45f27d0188bc8cdaf8b219f6b01845c9ac
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-03-20 02:41:01 +0530

    [fix]:fixed minor changes

--- src/App.js
+++ src/App.js
@@ -1,41 +1,27 @@
 import "./App.css";
-import logo from "./logo.png";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Header from "./components/Header";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="mockBee logo" width="180" height="180" />
-        <h1 className="brand-title">
-          Welcome to <span>mockBee!</span>
-        </h1>
-        <p className="brand-description">
-          Get started by editing <code>src/App.js</code>
-        </p>
-        <div className="links">
-          <a
-            href="https://mockbee.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Explore mockBee
-          </a>
-          <a
-            href="https://mockbee.netlify.app/docs/api/introduction"
-            target="_blank"
-            rel="noreferrer"
-          >
-            API Documentation
-          </a>
-          <a
-            href="https://github.com/neogcamp/mockBee"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Contribute
-          </a>
-        </div>
-      </header>
+    <div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                <Home />
+                <Footer />
+              </div>
+            }
+          ></Route>
+          <Route path="/products" ></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import {Link} from "react-router-dom"
+
+export default function Header() {
+  return (
+    <div>
+      <>
+        <header className="flex-row header-bar">
+          <div className="flex-row header-left">
+            <i className="material-icons header-icon"> menu </i>
+            <Link to="/" className="hm-pg-link">
+              <h1 className="flex-row web-logo">
+                <span className="material-icons web-logo-icon">
+                  {" "}
+                  sports_esports{" "}
+                </span>
+                GameKart
+              </h1>
+            </Link>
+          </div>
+          <div className="flex-row header-mid">
+            <i className="material-icons"> search </i>
+            <input
+              type="text"
+              placeholder="Search varierty gaming products.."
+              className="search-bar"
+            />
+          </div>
+          <a href="./screens/LoginPage.html">
+            <button className="btn primary-btn">LOGIN</button>
+          </a>
+          <div className="flex-row header-right">
+            <a href="./screens/wishlist.html">
+              <div className="relative-container">
+                <i className="material-icons header-icon">
+                  favorite_border
+                  <div className="badge top-right lrg red-clr flex-row center-it">
+                    3
+                  </div>
+                </i>
+              </div>
+            </a>
+            <a href="./screens/mycart.html" className="cart">
+              <div className="flex-row relative-container">
+                <i className="material-icons header-icon"> shopping_cart </i>
+                <div className="badge top-right lrg red-clr flex-row center-it">
+                  2
+                </div>
+              </div>
+            </a>
+            <i className="material-icons header-icon"> logout </i>
+          </div>
+        </header>
+      </>
+    </div>
+  );
+}
