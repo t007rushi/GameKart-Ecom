a85046ee8c447015b62a52067116767dd9641286
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-03-23 13:45:48 +0530

    gets all items of the wishlist from the db success

--- src/Context/wishlist-context.js
+++ src/Context/wishlist-context.js
@@ -1,12 +1,28 @@
 import { createContext, useContext, useState } from "react";
+import axios from "axios";
+import { useAuth } from "./auth-context";
 
 const wishlistContext = createContext();
 
 const WishlistProvider = ({ children }) => {
-  const [wishlist, setWishlist] = useState();
+  const [wishlist, setWishlist] = useState([]);
+  const { user } = useAuth();
+
+  const loadWishlist = async () => {
+    try {
+      const { data } = await axios.get("api/user/wishlist", {
+        headers: {
+          authorization: user.tokenVal,
+        },
+      });
+      setWishlist(data.wishlist);
+    } catch (error) {
+      console.error("wishlist error", error);
+    }
+  };
 
   return (
-    <wishlistContext.Provider value={{ wishlist, setWishlist }}>
+    <wishlistContext.Provider value={{ wishlist, setWishlist, loadWishlist }}>
       {children}
     </wishlistContext.Provider>
   );
