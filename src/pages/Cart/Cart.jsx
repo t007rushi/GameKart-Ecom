251324f72fe062cb0fa7dd8930dc05e255e8b5c9
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-03-26 16:28:59 +0530

    Cart Price details card added

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import {CartItems,PriceDetail} from "../../components/index"
+import { useCart } from "../../Context/cart-context";
 
 const Cart = () => {
+  const {cart} = useCart();
   return (
-    <div style={{ marginTop: "10rem" }}>
-      <h1> inside cart</h1>
-    </div>
+    <main className="cart-container">
+    <h1 className="cart-heading">My Cart items:({cart.length})</h1>
+    <section className="cart-wrap flex-row center-it gap-btwn">
+      <CartItems />
+      <PriceDetail />
+      <Link to="/products">back to products</Link>
+    </section>
+  </main>
+ 
   );
 };
 
