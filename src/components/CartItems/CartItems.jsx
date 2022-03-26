ee60834baf2c4a7bd5f58a4c1da9416ab9bddd92
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-03-26 17:00:25 +0530

    ```remove from cart functionality added"

--- src/components/CartItems/CartItems.jsx
+++ src/components/CartItems/CartItems.jsx
@@ -3,11 +3,9 @@ import { useCart } from "../../Context/cart-context";
 import { useWishlist } from "../../Context/wishlist-context";
 
 const CartItems = () => {
-  const {
-    cart,removeFromCart
-  } = useCart();
+  const { cart, removeFromCart } = useCart();
 
-  const {addToWishlist } = useWishlist();
+  const { addToWishlist } = useWishlist();
   return (
     <div className="flex-col">
       {cart.map((cartItem) => {
@@ -25,34 +23,28 @@ const CartItems = () => {
               </span>
               <div className="flex-row center-it gap-btwn">
                 <p>Quantity:</p>
-                <button
-                  className="cart-quant-btn"
-                  onClick={() =>{}
-                  }
-                >
+                <button className="cart-quant-btn" onClick={() => {}}>
                   -
                 </button>
                 <div className="cart-quant-cnt">{cartItem.quantity}</div>
-                <button
-                  className="cart-quant-btn"
-                  onClick={() =>{}
-                  }
-                >
+                <button className="cart-quant-btn" onClick={() => {}}>
                   +
                 </button>
               </div>
               <button
                 className="card-btn primary-btn"
-                onClick={() => {addToWishlist(cartItem)
-                  removeFromCart(cartItem._id)
+                onClick={() => {
+                  addToWishlist(cartItem);
+                  removeFromCart(cartItem._id);
                 }}
               >
                 Move to Wishlist
               </button>
               <button
                 className="card-btn card-icon-btn"
-                onClick={() =>{}
-                }
+                onClick={() => {
+                  removeFromCart(cartItem._id);
+                }}
               >
                 Remove from
                 <span className="material-icons icon">shopping_cart</span>
