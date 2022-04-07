17251b23d3c9d147de6599193b0823a26b83d5de
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-04-07 20:21:13 +0530

    category data added

---
+++ src/data/categoryData.js
@@ -0,0 +1,15 @@
+export const categoryData = [
+  {
+    icon: "sports_esports",
+    category: "GAMES",
+  },
+  {
+    icon: "kitchen",
+    category: "CONSOLE",
+  },
+  {
+    icon: "headphones",
+    category: "ACCESORIES",
+  },
+
+];
